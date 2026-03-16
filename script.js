87840e610ab3bc077474bc5cd664a60451edf575
YF3RSN <yefersonalejandroheredia38@gmail.com>
2026-03-16 13:56:47 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===== LISTA DE NÚMEROS VENDIDOS (debe ser la misma que en script-inicio.js) =====
-const vendidosOficiales = []; // ¡Actualiza aquí también!
+const vendidosOficiales = [11,89, 47, 520, 477,48]; // ¡Actualiza aquí también!
 const totalNumeros = 1000;
 const miTelefono = "584122415696"; // <--- CAMBIA AQUÍ A TU NÚMERO REAL (código país + número sin 0)
 
